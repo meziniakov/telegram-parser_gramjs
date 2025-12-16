ba1a43e267c6021c2b7d57f9c5e67a03103d7e81
Iakov Mezin <45094209+meziniakov@users.noreply.github.com>
2025-12-16 07:03:53 +0000

    generate-session

---
+++ src/generate-session.js
@@ -0,0 +1,25 @@
+const { TelegramClient } = require('telegram');
+const { StringSession } = require('telegram/sessions');
+const input = require('input');
+
+const apiId = process.env.TELEGRAM_API_ID;
+const apiHash = process.env.TELEGRAM_API_HASH;
+
+(async () => {
+  const client = new TelegramClient(
+    new StringSession(''),
+    apiId,
+    apiHash,
+    { connectionRetries: 5 }
+  );
+
+  await client.start({
+    phoneNumber: async () => await input.text('Номер телефона: '),
+    password: async () => await input.text('Пароль 2FA (если есть): '),
+    phoneCode: async () => await input.text('Код из Telegram: '),
+    onError: (err) => console.log(err),
+  });
+
+  console.log('Session String:');
+  console.log(client.session.save());
+})();
